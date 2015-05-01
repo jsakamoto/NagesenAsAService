c94d9e92b5b59d92aa216228f3033ff00e7ae7ab
jsakamoto <junichi.jp@gmail.com>
2015-05-02 00:04:25 +0900

    Improve share box UI between modern browsers.

--- NagesenAsAService/Scripts/sharebox.js
+++ NagesenAsAService/Scripts/sharebox.js
@@ -2,20 +2,19 @@ var NaaS;
 (function (NaaS) {
     // "Share" box and QR Code - UI effect
     $(function () {
-        $(document).on('click', '.modal-mask.active,.share-box', function () {
+        $(document).on('click', '.qr-code-icon,.share-box,.modal-mask.active', function (e) {
+            e.preventDefault();
             $('.share-box,.modal-mask').toggleClass('active');
-            $('.modal-mask').fadeToggle();
+            $('.share-box,.modal-mask').fadeToggle();
         });
         $('.share-box #buttonTweet').click(function (e) {
+            e.preventDefault();
             e.stopPropagation();
             var text = "投げ銭 as a Service - Room " + _app.roomNumber + " に今すぐアクセス☆";
-            var url = 'https://twitter.com/share?';
+            var url = _app.apiBaseUrl + '/TwitterShare?';
             url += 'text=' + encodeURIComponent(text);
-            url += '&url=' + encodeURIComponent(_app.controllerUrl);
-            $.get(_app.twitterHashtagUrl).done(function (data) {
-                url += '&hashtags=' + encodeURIComponent(data.twitterHashtag);
-                window.open(url, 'tweet');
-            });
+            url += '&url=' + encodeURIComponent(_app.apiBaseUrl);
+            window.open(url);
         });
     });
 })(NaaS || (NaaS = {}));
