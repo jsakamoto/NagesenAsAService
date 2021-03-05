16fddb1d9a5e601a1b269616a5640557d43e4369
jsakamoto <junichi.jp@gmail.com>
2021-03-05 19:11:51 +0900

    Azure SignalR サービスを利用可能とするよう実装

--- NagesenAsAService/Startup.cs
+++ NagesenAsAService/Startup.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using NagesenAsAService.Hubs;
@@ -8,13 +9,23 @@ namespace NagesenAsAService
 {
     public class Startup
     {
+        private IConfiguration Configuration { get; }
+
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
             services.AddRazorPages();
-            services.AddSignalR();
+
+            var signalRServerBuilder = services.AddSignalR();
+            if (!string.IsNullOrEmpty(this.Configuration["Azure:SignalR:ConnectionString"]))
+                signalRServerBuilder.AddAzureSignalR();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
