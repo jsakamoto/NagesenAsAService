574912a2ad2d1eb358679cb575b3f91d2a76fe94
jsakamoto <junichi.jp@gmail.com>
2021-03-07 15:57:54 +0900

    ローカライズ機構を移植、有効化

--- NagesenAsAService/Startup.cs
+++ NagesenAsAService/Startup.cs
@@ -50,6 +50,12 @@ namespace NagesenAsAService
             app.UseStaticFiles();
             app.UseRouting();
 
+            var supportedCultures = new[] { "en", "ja" };
+            var localizationOptions = new RequestLocalizationOptions().SetDefaultCulture(supportedCultures[0])
+                .AddSupportedCultures(supportedCultures)
+                .AddSupportedUICultures(supportedCultures);
+            app.UseRequestLocalization(localizationOptions);
+
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapRazorPages();
