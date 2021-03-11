b5b9b79551ad6b7a1ad537882d167befdcac9dba
jsakamoto <junichi.jp@gmail.com>
2021-03-11 18:30:44 +0900

    開発用のインメモリのルームリポジトリについて、ローカルの JSON ファイルに永続化するよう改善
    - ビルドするごとにルームが揮発してしまうと開発ができないため

--- NagesenAsAService/Services/RoomRepository/InMemoryRoomRepository.cs
+++ NagesenAsAService/Services/RoomRepository/InMemoryRoomRepository.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Text.Json;
 using System.Threading.Tasks;
-
+using Microsoft.AspNetCore.Hosting;
 using NagesenAsAService.Models;
 
 namespace NagesenAsAService.Services.RoomRepository
@@ -13,14 +15,32 @@ namespace NagesenAsAService.Services.RoomRepository
 
         private Dictionary<Guid, Picture> Pictures { get; } = new Dictionary<Guid, Picture>();
 
-        public InMemoryRoomRepository()
+        private string RoomsStoragePath { get; }
+
+        public InMemoryRoomRepository(IWebHostEnvironment webHostEnv)
         {
+            var saveDir = Path.Combine(webHostEnv.ContentRootPath, "App_Data");
+            if (!Directory.Exists(saveDir)) Directory.CreateDirectory(saveDir);
+
+            this.RoomsStoragePath = Path.Combine(saveDir, "rooms.json");
+            if (File.Exists(this.RoomsStoragePath))
+            {
+                var roomsJsonStr = File.ReadAllText(this.RoomsStoragePath);
+                this.Rooms = JsonSerializer.Deserialize<List<Room>>(roomsJsonStr);
+            }
         }
 
-        public Task AddRoomAsync(Room room)
+        public async Task AddRoomAsync(Room room)
         {
             lock (this.Rooms) this.Rooms.Add(room);
-            return Task.CompletedTask;
+            await this.SaveRoomsAsync();
+        }
+
+        private async Task SaveRoomsAsync()
+        {
+            var roomsJsonStr = default(string);
+            lock (this.Rooms) roomsJsonStr = JsonSerializer.Serialize(this.Rooms);
+            await File.WriteAllTextAsync(this.RoomsStoragePath, roomsJsonStr);
         }
 
         public Task<Room> FindRoomAsync(int roomNumber)
@@ -60,22 +80,26 @@ namespace NagesenAsAService.Services.RoomRepository
             }
         }
 
-        public Task SweepRoomsAsync(DateTime limit)
-        {
-            // NOP
-            return Task.CompletedTask;
-        }
-
-        public Task<Room> UpdateRoomAsync(int roomNumber, Func<Room, bool> action)
+        public async Task SweepRoomsAsync(DateTime limit)
         {
             lock (this.Rooms)
             {
-                var room = this.Rooms.FirstOrDefault(r => r.RoomNumber == roomNumber);
-                if (room == null) return Task.FromResult(room);
-
-                action(room);
-                return Task.FromResult(room);
+                var roomsToSweep = this.Rooms.Where(room => room.CreatedAt < limit).ToArray();
+                foreach (var roomToSweep in roomsToSweep) this.Rooms.Remove(roomToSweep);
             }
+            await this.SaveRoomsAsync();
+        }
+
+        public async Task<Room> UpdateRoomAsync(int roomNumber, Func<Room, bool> action)
+        {
+            var room = default(Room);
+            lock (this.Rooms)
+            {
+                room = this.Rooms.FirstOrDefault(r => r.RoomNumber == roomNumber);
+                if (room != null) action(room);
+            }
+            await this.SaveRoomsAsync();
+            return room;
         }
     }
 }
