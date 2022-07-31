b65335b5281a4a31566c2cb24ce4c69179ef247b
jsakamoto <junichi.jp@gmail.com>
2022-07-31 09:54:33 +0900

    クライアントサンプルコード - .NET コンソールアプリによるクライアント - コードを整理

--- Client Samples/DotNetConsoleApp/Program.cs
+++ Client Samples/DotNetConsoleApp/Program.cs
@@ -1,37 +1,38 @@
 ﻿using Microsoft.AspNetCore.SignalR.Client;
+using static System.Console;
 
-Console.WriteLine("投げ銭BOX as a Service に接続中...");
+WriteLine("投げ銭BOX as a Service に接続中...");
 var connection = new HubConnectionBuilder()
     .WithUrl("https://naas.azurewebsites.net/naashub")
     .WithAutomaticReconnect()
     .Build();
 await connection.StartAsync();
-Console.WriteLine("接続しました。");
+WriteLine("接続しました。");
 
-Console.Write("4桁の「部屋番号」を入力: ");
-var roomNumber = int.Parse(Console.ReadLine() ?? "");
+Write("4桁の「部屋番号」を入力: ");
+var roomNumber = int.Parse(ReadLine() ?? "");
 var roomContext = await connection.InvokeAsync<RoomContext>("EnterRoomAsBoxAsync", roomNumber);
-Console.WriteLine($"部屋番号 {roomNumber} に入室しました。");
-Console.WriteLine($"- Like 銭: {roomContext.CountOfLike}");
-Console.WriteLine($"- Dis 銭 : {roomContext.CountOfDis}");
+WriteLine($"部屋番号 {roomNumber} に入室しました。");
+WriteLine($"- Like 銭: {roomContext.CountOfLike}");
+WriteLine($"- Dis 銭 : {roomContext.CountOfDis}");
 
-Console.WriteLine("投げ銭投入の通知を受信します。");
+WriteLine("投げ銭投入の通知を受信します。");
 connection.On("Throw", async (ThrowCoinEventArgs args) =>
 {
-    Console.WriteLine($"{args.TypeOfCoin} 銭が投入されました。");
-    Console.WriteLine($"- Like 銭: {args.CountOfLike}");
-    Console.WriteLine($"- Dis 銭 : {args.CountOfDis}");
+    WriteLine($"{args.TypeOfCoin} 銭が投入されました。");
+    WriteLine($"- Like 銭: {args.CountOfLike}");
+    WriteLine($"- Dis 銭 : {args.CountOfDis}");
     await Task.CompletedTask;
 });
 
-Console.WriteLine("投げ銭を待機中。止めるには Ctrl + C を押して下さい。");
+WriteLine("投げ銭を待機中。止めるには Ctrl + C を押して下さい。");
 var canceller = new CancellationTokenSource();
-Console.CancelKeyPress += (_, args) => { args.Cancel = true; canceller.Cancel(); };
+CancelKeyPress += (_, args) => { args.Cancel = true; canceller.Cancel(); };
 canceller.Token.WaitHandle.WaitOne();
 
-Console.WriteLine("切断中...");
+WriteLine("切断中...");
 await connection.StopAsync();
-Console.WriteLine("切断しました。");
+WriteLine("切断しました。");
 
 // 以下は型宣言
 record RoomContext(int CountOfLike, int CountOfDis);
